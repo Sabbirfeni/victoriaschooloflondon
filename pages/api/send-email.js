860438e492710a22dfe87ac1128ce17008116f50
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2025-04-14 19:05:22 +0600

    configuration added to enable post request

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -17,6 +17,11 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    return res.status(200).end();
+  }
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
