e80b4f364dbd8f79db100379dc941046eaaa999d
Jahir Ahammed <96808237+jahirahammedron@users.noreply.github.com>
2025-04-21 12:58:02 +0530

    Update universities.js

--- database/universities.js
+++ database/universities.js
@@ -1,6 +1,12 @@
 const universities = [
   
-  
+    {
+    country: "United Kingdom",
+    name: "Birmingham City University",
+    logo: "/Birmingham_City_University_Logo.png",
+    profile:"https://victoriaschooloflondon.co.uk/university-finding-2/birmingham-city-university/",
+  },
+
   
 ];
 
